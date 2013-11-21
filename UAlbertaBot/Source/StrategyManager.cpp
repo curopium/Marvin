8bbcd461522bf673a7902d35c182a12cec1d28ad
Thomas Curnow <curnow@ualberta.ca>
2013-11-20 18:25:41 -0700

    Implemented the Dreaded 4 pool
    Its still got it

--- UAlbertaBot/Source/StrategyManager.cpp
+++ UAlbertaBot/Source/StrategyManager.cpp
@@ -32,7 +32,7 @@ void StrategyManager::addStrategies()
     protossOpeningBook[ProtossDarkTemplar]	=     "0 0 0 0 1 0 3 0 7 0 5 0 12 0 13 3 22 22 1 22 22 0 1 0";
 	protossOpeningBook[ProtossDragoons]		= "0 0 0 0 1 0 0 3 0 7 0 0 5 0 0 3 8 6 1 6 6 0 3 1 0 6 6 6";
 	terranOpeningBook[TerranMarineRush]		= "0 0 0 0 0 1 0 0 3 0 0 3 0 1 0 4 0 0 0 6";
-	zergOpeningBook[ZergZerglingRush]		= "0 0 0 0 0 1 0 0 0 2 3 5 0 0 0 0 0 0 1 6";
+	zergOpeningBook[ZergZerglingRush]		= "0 3 4 4 4";
 
 	if (selfRace == BWAPI::Races::Protoss)
 	{
